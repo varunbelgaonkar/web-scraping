1ba0217a4fce19f00c3258e5616b5cda101a94fc
varunbelgaonkar <66586863+varunbelgaonkar@users.noreply.github.com>
2021-01-15 12:08:53 +0530

    Update main.py
    Added comments

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+
+#creating a csv file
 filename = "components.csv"
 f = open(filename,'w') 
 headers = "Product_name, Price\n"
 f.write(headers)
+
 url = 'https://www.electronicscomp.com/sensors-module/modules/gsm-gps-module'
-page = requests.get(url)
-if page.status_code == 200:
+page = requests.get(url)   #Getting the reponse from web page
+if page.status_code == 200:    #checking if the request is successful
   soup = BeautifulSoup(page.content, 'html.parser')
   product_container = soup.find_all('div', class_ = 'product-layout product-list col-xs-12')
   for product in product_container:
