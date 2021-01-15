d0edde5163fc21c98ed5e0064b9b812089f00c8a
varunbelgaonkar <66586863+varunbelgaonkar@users.noreply.github.com>
2021-01-15 11:22:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+filename = "components.csv"
+f = open(filename,'w') 
+headers = "Product_name, Price\n"
+f.write(headers)
+url = 'https://www.electronicscomp.com/sensors-module/modules/gsm-gps-module'
+page = requests.get(url)
+if page.status_code == 200:
+  soup = BeautifulSoup(page.content, 'html.parser')
+  product_container = soup.find_all('div', class_ = 'product-layout product-list col-xs-12')
+  for product in product_container:
+    product_name = product.find('div', class_ = 'styleh4categ').string
+    price = product.find('div', class_ = 'price-tab-main').find('div', class_ = 'price').span.string
+    print("Product_name: " + product_name)
+    print("Price: " + price)
+    f.write(product_name + "," + price + "\n")
+
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
